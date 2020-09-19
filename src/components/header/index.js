a6b3498e8b2f40545f65e9e7361aefa0b2c04786
Edison <edi9708@hotmail.com>
2020-09-19 11:01:03 -0500

    add styles and examples

---
+++ src/components/header/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Contenedor } from './style';
+
+class Header extends Component {
+    render() {
+        return (
+            <Contenedor>
+                <h1> MENU s</h1>
+            </Contenedor>
+        )
+    }
+}
+
+export default Header
---
+++ src/components/header/style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Contenedor = styled.div`
+	width: 100%;
+	height: 100%;
+	background-color: red;
+`;
