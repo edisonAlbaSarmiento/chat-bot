ed286c9c6d7b5243006cced14496d17ecf7fd1ea
Edison <edi9708@hotmail.com>
2020-09-25 22:01:47 -0500

    update in styles

--- src/components/body/index.js
+++ src/components/body/index.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
-
+import { 
+	Container,
+	ContainerHeader,
+	ContainerText,
+	Title,
+	Subtitle,
+	DataTitle,
+	DataSubTitle,
+	ContainerDescription,
+	Button,
+} from './styles'
 class Body extends Component {
     render() {
         return (
-            <div>
-                <h1> Body </h1>
-            </div>
+            <Container>
+							<ContainerHeader>
+								<ContainerText>
+									<ContainerDescription>
+									<Title> 2020 </Title>
+									<Subtitle>EDITION</Subtitle>
+									<DataTitle> March 7</DataTitle>
+									<DataSubTitle>Carnival party</DataSubTitle>
+									<p>Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit dolor sit amet. </p>
+									<Button>
+										Read more 
+									</Button>
+									</ContainerDescription>
+								
+								</ContainerText>
+
+							</ContainerHeader>
+            </Container>
         )
     }
 }
---
+++ src/components/body/styles.js
@@ -0,0 +1,72 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: rgb(225, 44, 55);
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  padding-top: 3%;
+  padding-bottom: 3%;
+`;
+
+export const ContainerHeader = styled.div`
+  background: url(//themes.muffingroup.com/be/party3/wp-content/uploads/2020/03/party3-slider-bg.jpg) center center / cover no-repeat;
+  width: 1530px;
+  height: 100%;
+  border-radius: 30px;
+  display: flex;
+  justify-content: center;
+`;
+
+export const ContainerText = styled.div`
+  padding: 6%;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Title = styled.h1`
+  font-size: 20px;
+  color: rgb(225, 44, 55)
+`;
+
+export const Subtitle = styled.h3`
+  margin-top: -15px;
+  font-size: 14px;
+`;
+export const DataTitle = styled.h1`
+  font-size: 48px;
+  color: rgb(225, 44, 55);
+`;
+
+
+export const DataSubTitle = styled.h3`
+  margin-top: -26px;
+  font-size: 48px;
+`;
+
+export const ContainerDescription = styled.div`
+  width: 45%; 
+`;
+
+export const Button = styled.button`
+  margin-top: 9%;
+  font-size: 12px;
+  border-radius: 3px;
+  background-color: rgb(28, 9, 5);
+  color: rgb(255, 255, 255);
+  border-radius: 25px;
+  padding: 0px 24px;
+  line-height: 34px;
+  border-style: none;
+  outline: none;
+  margin-left: 0px;
+  transition: 1s;
+  &:hover {
+    background-color:  rgb(225, 44, 55);
+    margin-left: -10px;
+    transition: 1s;
+  }
+
+`;
