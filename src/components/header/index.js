56c863b068bbe1c394908236ab2bc197f9c97b0a
divelasquez <30845765+divelasquez@users.noreply.github.com>
2020-09-26 10:55:40 -0500

    Update index.js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
 import { Contenedor } from './style';
+import { ContenedorMenu } from './style';
+import { ContenedorLinea } from './style';
+import { ContenedorLogo } from './style';
+
 
 class Header extends Component {
     render() {
         return (
             <Contenedor>
-                <h1> MENU s</h1>
+                 <ContenedorLogo>   
+                    <div>
+                        <img src="https://themes.muffingroup.com/be/party3/wp-content/uploads/2020/02/party3.png" data-height="100" alt="party3" data-no-retina="">
+                            </img>  
+                    </div>  
+                </ContenedorLogo>                     
+                <ContenedorMenu>  
+                    <div>                   
+                        <ContenedorLinea>                        
+                        Inicio
+                        Nosotros
+                        Galeria
+                        Contacto                     
+                        </ContenedorLinea> 
+                    </div>  
+                </ContenedorMenu>  
             </Contenedor>
         )
     }
