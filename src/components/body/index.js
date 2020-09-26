282a0792f7e0b93491c79ccf733e86bd90d25907
Edison <edi9708@hotmail.com>
2020-09-26 10:43:00 -0500

    change code

--- src/components/body/index.js
+++ src/components/body/index.js
@@ -17,27 +17,24 @@ class Body extends Component {
     render() {
         return (
             <Container>
-							<ContainerHeader>
-								<ContainerText>
-									<ContainerDescription>
-									<Title> 2020 </Title>
-									<Subtitle>EDITION</Subtitle>
-									<DataTitle> March 7</DataTitle>
-									<DataSubTitle>Carnival party</DataSubTitle>
-									<p>Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit dolor sit amet. </p>
-									<Button>
-										Read more 
-									</Button>
-									</ContainerDescription>
-								
-								</ContainerText>
-
-							</ContainerHeader>
-							<ContainerScroll>
-								<ButtonSecond src="https://themes.muffingroup.com/be/party3/wp-content/uploads/2020/02/party3-icon1.png" alt="party3-icon1" title="party3-icon1"/>
-								<TextButtonSecond>SCROLL DOWN</TextButtonSecond>
-							</ContainerScroll>
-						
+				<ContainerHeader>
+					<ContainerText>
+						<ContainerDescription>
+						<Title> 2020 </Title>
+						<Subtitle>EDITION</Subtitle>
+						<DataTitle> March 7</DataTitle>
+						<DataSubTitle>Carnival party</DataSubTitle>
+						<p>Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit dolor sit amet. </p>
+						<Button>
+							Read more 
+						</Button>
+						</ContainerDescription>
+					</ContainerText>
+				</ContainerHeader>
+				<ContainerScroll>
+					<ButtonSecond src="https://themes.muffingroup.com/be/party3/wp-content/uploads/2020/02/party3-icon1.png" alt="party3-icon1" title="party3-icon1"/>
+					<TextButtonSecond>SCROLL DOWN</TextButtonSecond>
+				</ContainerScroll>
             </Container>
         )
     }
