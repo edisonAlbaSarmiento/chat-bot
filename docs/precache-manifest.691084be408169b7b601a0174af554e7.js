40926e27f8a7a629dab79956ace7681528a96158
Edison <edi9708@hotmail.com>
2020-11-08 12:23:54 -0500

    add deploy

---
+++ docs/precache-manifest.691084be408169b7b601a0174af554e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb8b97146c236ddb484e96fbc763c56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac31a8fd2b820326828e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7dacb66e348a54e66ad1",
+    "url": "/static/js/2.7a975ffe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7a975ffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac31a8fd2b820326828e",
+    "url": "/static/js/main.42e5eed1.chunk.js"
+  },
+  {
+    "revision": "0e5dc210f421aea4b19a",
+    "url": "/static/js/runtime-main.7aeb85a8.js"
+  }
+]);
